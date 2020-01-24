13cae7a631a84a2fdb3a8f0a8a47191ed1d5610d
voravitl <voravitl@gmail.com>
2020-01-25 01:10:49 +0700

    mvn version

--- bin/setup_jenkins_slave.sh
+++ bin/setup_jenkins_slave.sh
@@ -9,8 +9,9 @@ oc new-build --strategy=docker -D $'FROM quay.io/openshift/origin-jenkins-agent-
    RUN curl https://copr.fedorainfracloud.org/coprs/alsadi/dumb-init/repo/epel-7/alsadi-dumb-init-epel-7.repo -o /etc/yum.repos.d/alsadi-dumb-init-epel-7.repo && \ \n
    curl https://raw.githubusercontent.com/cloudrouter/centos-repo/master/CentOS-Base.repo -o /etc/yum.repos.d/CentOS-Base.repo && \ \n
    curl http://mirror.centos.org/centos-7/7/os/x86_64/RPM-GPG-KEY-CentOS-7 -o /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7 && \ \n
-   curl -L -o /tmp/apache-maven-3.6.3-bin.zip https://www-eu.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip && \ \n
-   unzip /tmp/apache-maven-3.6.3-bin.zip \ \n
+   curl -L -o /tmp/apache-maven-3.6.3-bin.tar.gz https://www-eu.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz && \ \n
+   gzip -d /tmp/apache-maven-3.6.3-bin.tar.gz && \ \n
+   tar -xf /tmp/apache-maven-3.6.3-bin.tar && \ \n
    DISABLES="--disablerepo=rhel-server-extras --disablerepo=rhel-server --disablerepo=rhel-fast-datapath --disablerepo=rhel-server-optional --disablerepo=rhel-server-ose --disablerepo=rhel-server-rhscl" && \ \n
    yum $DISABLES -y --setopt=tsflags=nodocs install skopeo && yum clean all\n
    USER 1001' --name=${JENKINS_SLAVE}
