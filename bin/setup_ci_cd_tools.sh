0a044ea56244ee964eb7e00ab5e68e3a3ea15bfa
voravitl <voravitl@gmail.com>
2020-01-29 17:33:30 +0700

    Update main README.adoc

--- bin/setup_ci_cd_tools.sh
+++ bin/setup_ci_cd_tools.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
-oc new-project ci-cd --display-name="CI/CD Tools"
-oc new-app sonatype/nexus3:latest --name=nexus
-oc expose svc nexus
-oc rollout pause dc nexus
-oc patch dc nexus --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'
-oc set resources dc nexus --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m
-oc set volume dc/nexus --add --overwrite --name=nexus-pv-1 --mount-path=/nexus-data/ --type persistentVolumeClaim --claim-name=nexus-pvc --claim-size=4Gi
-oc set probe dc/nexus --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok
-oc set probe dc/nexus --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/
-oc rollout resume dc nexus 
-echo "****** Press Ctrl-C when nexus pod is started *****"
-watch oc get pods
+function check_pod(){
+    clear
+    echo "****** Wait for $1 pod to sucessfully start ******"
+    sleep 10
+    READY="NO"
+    while [ $READY = "NO" ];
+    do
+        MESSAGE=$(oc get pods  -n ${CICD_PROJECT}| grep $1 | grep -v deploy)
+        STATUS=$(echo ${MESSAGE}| awk '{print $2}')
+        echo "Current Status: ${MESSAGE}"
+        if [ ${STATUS} = "1/1" ];
+        then
+            READY="YES"
+        else 
+            echo "Wait for 5 sec"
+            sleep 5
+        fi
+    done
+}
+CICD_PROJECT=a3
+oc new-project ${CICD_PROJECT} --display-name="CI/CD Tools"
+oc new-app sonatype/nexus3:latest --name=nexus -n ${CICD_PROJECT}
+oc expose svc nexus -n ${CICD_PROJECT}
+oc rollout pause dc nexus -n ${CICD_PROJECT}
+oc patch dc nexus --patch='{ "spec": { "strategy": { "type": "Recreate" }}}' -n ${CICD_PROJECT}
+oc set resources dc nexus --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m -n ${CICD_PROJECT}
+oc set volume dc/nexus --add --overwrite --name=nexus-pv-1 --mount-path=/nexus-data/ --type persistentVolumeClaim --claim-name=nexus-pvc --claim-size=4Gi -n ${CICD_PROJECT}
+oc set probe dc/nexus --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok -n ${CICD_PROJECT}
+oc set probe dc/nexus --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/ -n ${CICD_PROJECT}
+oc rollout resume dc nexus -n ${CICD_PROJECT}
+check_pod "nexus"
 export NEXUS_POD=$(oc get pods | grep nexus | grep -v deploy | awk '{print $1}')
 export NEXUS_PASSWORD=$(oc rsh $NEXUS_POD cat /nexus-data/admin.password)
 #curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/redhat-gpte-devopsautomation/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
@@ -19,7 +38,7 @@ export NEXUS_PASSWORD=$(oc rsh $NEXUS_POD cat /nexus-data/admin.password)
 oc expose dc nexus --port=5000 --name=nexus-registry
 oc create route edge nexus-registry --service=nexus-registry --port=5000
 oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db
-watch oc get pods
+check_pod "postgresql"
 oc new-app --docker-image=quay.io/gpte-devops-automation/sonarqube:7.9.1 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube
 oc rollout pause dc sonarqube
 oc expose service sonarqube
@@ -30,8 +49,10 @@ oc set probe dc/sonarqube --liveness --failure-threshold 3 --initial-delay-secon
 oc set probe dc/sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url=http://:9000/about
 oc patch dc/sonarqube --type=merge -p '{"spec": {"template": {"metadata": {"labels": {"tuned.openshift.io/elasticsearch": "true"}}}}}'
 oc rollout resume dc sonarqube
+check_pod "sonarqube"
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
 oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m
+check_pod "jenkins"
 oc new-build --strategy=docker -D $'FROM quay.io/openshift/origin-jenkins-agent-maven:4.1.0\n
    USER root\n
    RUN curl https://copr.fedorainfracloud.org/coprs/alsadi/dumb-init/repo/epel-7/alsadi-dumb-init-epel-7.repo -o /etc/yum.repos.d/alsadi-dumb-init-epel-7.repo && \ \n
