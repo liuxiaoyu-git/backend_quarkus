b6680ad10d7f150dd7dc22174104b91f13a49c9f
voravitl <voravitl@gmail.com>
2020-01-31 18:46:43 +0700

    Add bash scripts for run pipelines

---
+++ bin/create_pipelines.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+CICD_PROJECT=ci-cd
+oc apply -f ../manifests/backend-build-pipeline.yaml -n ${CICD_PROJECT}
+oc apply -f ../manifests/backend-release-pipeline.yaml -n ${CICD_PROJECT}
+oc apply -f ../manifests/backend-release-uat-pipeline.yaml -n ${CICD_PROJECT}
+oc apply -f ../manifests/backend-release-prod-pipeline.yaml -n ${CICD_PROJECT}
