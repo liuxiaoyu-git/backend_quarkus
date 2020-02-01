8baf314e91bfe7e18b89ed3fab45d37844a087d8
voravitl <voravitl@gmail.com>
2020-02-01 16:59:50 +0700

    quarkus native build

--- bin/setup_maven36_graalvm_slave.sh
+++ bin/setup_maven36_graalvm_slave.sh
@@ -16,13 +16,14 @@ oc new-build --strategy=docker -D $'FROM quay.io/openshift/origin-jenkins-agent-
    gzip -d /tmp/graalvm-ce-linux-amd64-19.2.1.tar.gz && \ \n
    tar -C /opt -xf /tmp/graalvm-ce-linux-amd64-19.2.1.tar && \ \n
    rm -f /tmp/graalvm-ce-linux-amd64-19.2.1.tar && \ \n
-   /opt/graalvm-ce-19.2.1/bin/gu install native-image && \ \n
-   chmod -R 755 /opt/graalvm-ce-19.2.1 && \ \n
-   chown -R 1001:0 /opt/graalvm-ce-19.2.1 && \ \n
    DISABLES="--disablerepo=rhel-server-extras --disablerepo=rhel-server --disablerepo=rhel-fast-datapath --disablerepo=rhel-server-optional --disablerepo=rhel-server-ose --disablerepo=rhel-server-rhscl" && \ \n
    yum $DISABLES -y --setopt=tsflags=nodocs install skopeo && yum clean all\n
    ENV GRAALVM_HOME=/opt/graalvm-ce-19.2.1\n
    ENV PATH=/opt/apache-maven-3.6.3/bin:$GRAALVM_HOME/bin:$PATH\n
+   RUN export GRAALVM_HOME=/opt/graalvm-ce-19.2.1 && \ \n
+   /opt/graalvm-ce-19.2.1/bin/gu install native-image && \ \n
+   chmod -R 755 /opt/graalvm-ce-19.2.1 && \ \n
+   chown -R 1001:0 /opt/graalvm-ce-19.2.1\n
    USER 1001' --name=${JENKINS_SLAVE} -n ${PROJECT}
 echo "Wait 5 sec for build to start"
 sleep 5
