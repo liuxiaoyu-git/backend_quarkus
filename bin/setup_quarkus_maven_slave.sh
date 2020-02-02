9996fae23c92c94be813edfc5c865d4cf110bfed
voravitl <voravitl@gmail.com>
2020-02-02 16:14:54 +0700

    quarkus native build

--- bin/setup_quarkus_maven_slave.sh
+++ bin/setup_quarkus_maven_slave.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 PROJECT=ci-cd
 JENKINS_SLAVE=quarkus-maven-slave
-REGISTRY=quay.io
-IMAGE=quarkus/centos-quarkus-maven
-TAG=19.2.1
-oc import-image ${JENKINS_SLAVE}:${TAG} \
---from=${REGISTRY}/${IMAGE}:${TAG} -n ${PROJECT} \
---scheduled \
---confirm
+oc new-build --strategy=docker \
+-D $'FROM quay.io/quarkus/centos-quarkus-maven:19.2.1\n
+USER 1001' \
+--name=${JENKINS_SLAVE} -n ${PROJECT}
+echo "Wait 5 sec for build to start"
+sleep 5
+oc logs build/${JENKINS_SLAVE}-1 -f -n ${PROJECT}
+oc get build/${JENKINS_SLAVE}-1 -n ${PROJECT}
+# PROJECT=ci-cd
+# JENKINS_SLAVE=quarkus-maven-slave
+# REGISTRY=quay.io
+# IMAGE=quarkus/centos-quarkus-maven
+# TAG=19.2.1
+# oc import-image ${JENKINS_SLAVE}:${TAG} \
+# --from=${REGISTRY}/${IMAGE}:${TAG} -n ${PROJECT} \
+# --scheduled \
+# --confirm
+
+# FROM quay.io/quarkus/centos-quarkus-maven:19.2.1 AS build
+# COPY src /usr/src/app/src
+# COPY pom.xml /usr/src/app
+# USER root
+# RUN chown -R quarkus /usr/src/app
+# USER quarkus
+# RUN mvn -f /usr/src/app/pom.xml -Pnative clean package
