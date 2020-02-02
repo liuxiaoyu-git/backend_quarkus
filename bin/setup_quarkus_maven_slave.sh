3ac53a4827cf25687d57245b09c3e0fcff55266f
voravitl <voravitl@gmail.com>
2020-02-02 17:28:45 +0700

    quarkus native build

--- bin/setup_quarkus_maven_slave.sh
+++ bin/setup_quarkus_maven_slave.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-# PROJECT=ci-cd
-# JENKINS_SLAVE=quarkus-maven-slave
-# oc new-build --strategy=docker \
-# -D $'FROM quay.io/quarkus/centos-quarkus-maven:19.2.1\n
-# USER 1001' \
-# --name=${JENKINS_SLAVE} -n ${PROJECT}
-# echo "Wait 5 sec for build to start"
-# sleep 5
-# oc logs build/${JENKINS_SLAVE}-1 -f -n ${PROJECT}
-# oc get build/${JENKINS_SLAVE}-1 -n ${PROJECT}
 PROJECT=ci-cd
 JENKINS_SLAVE=quarkus-maven-slave
-REGISTRY=quay.io
-IMAGE=quarkus/centos-quarkus-maven
-TAG=19.2.1
-oc import-image ${JENKINS_SLAVE} \
---from=${REGISTRY}/${IMAGE}:${TAG} -n ${PROJECT} \
---scheduled \
---confirm
+oc new-build --strategy=docker \
+-D $'FROM quay.io/quarkus/centos-quarkus-maven:19.2.1\n
+   USER root\n
+   RUN curl https://copr.fedorainfracloud.org/coprs/alsadi/dumb-init/repo/epel-7/alsadi-dumb-init-epel-7.repo -o /etc/yum.repos.d/alsadi-dumb-init-epel-7.repo && \ \n
+   curl https://raw.githubusercontent.com/cloudrouter/centos-repo/master/CentOS-Base.repo -o /etc/yum.repos.d/CentOS-Base.repo && \ \n
+   curl http://mirror.centos.org/centos-7/7/os/x86_64/RPM-GPG-KEY-CentOS-7 -o /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7 && \ \n
+   yum -y --setopt=tsflags=nodocs  install skopeo && yum clean all\n
+   USER quarkus' --name=${JENKINS_SLAVE} -n ${PROJECT}
+echo "Wait 5 sec for build to start"
+sleep 5
+oc logs build/${JENKINS_SLAVE}-1 -f -n ${PROJECT}
+oc get build/${JENKINS_SLAVE}-1 -n ${PROJECT}
+# PROJECT=ci-cd
+# JENKINS_SLAVE=quarkus-maven-slave
+# REGISTRY=quay.io
+# IMAGE=quarkus/centos-quarkus-maven
+# TAG=19.2.1
+# oc import-image ${JENKINS_SLAVE} \
+# --from=${REGISTRY}/${IMAGE}:${TAG} -n ${PROJECT} \
+# --scheduled \
+# --confirm
 
 # FROM quay.io/quarkus/centos-quarkus-maven:19.2.1 AS build
 # COPY src /usr/src/app/src
