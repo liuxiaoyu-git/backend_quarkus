6ebe235a792068dfda218c52228b15e607c4f401
voravitl <voravitl@gmail.com>
2020-02-16 12:49:34 +0700

    Add test case for service,readiness and liveness

--- code/src/test/java/com/example/quarkus/ExampleResourceTest.java
+++ code/src/test/java/com/example/quarkus/ExampleResourceTest.java
@@ -10,12 +10,36 @@ import static org.hamcrest.CoreMatchers.is;
 public class ExampleResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testVersion() {
         given()
           .when().get("/version")
           .then()
              .statusCode(200);
              //.body(is("hello"));
+    }
+
+    @Test
+    public void testReadiness() {
+        given()
+          .when().get("/health/ready")
+          .then()
+             .statusCode(200);
+    }
+
+    @Test
+    public void testLiveness() {
+        given()
+          .when().get("/health/live")
+          .then()
+             .statusCode(200);
+    }
+    
+    @Test
+    public void testService() {
+        given()
+          .when().get("/")
+          .then()
+             .statusCode(200);
     }
 
 }
