be095b578c3d4a233d620d20ad8fc54883273150
voravitl <voravitl@gmail.com>
2020-02-18 11:22:35 +0700

    delete pipelines script

---
+++ bin/delete_pipelines.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+CICD_PROJECT=ci-cd
+oc delete -f ../manifests/backend-build-pipeline.yaml -n ${CICD_PROJECT}
+oc delete -f ../manifests/backend-release-pipeline.yaml -n ${CICD_PROJECT}
+oc delete -f ../manifests/backend-release-uat-pipeline.yaml -n ${CICD_PROJECT}
+oc delete -f ../manifests/backend-release-prod-pipeline.yaml -n ${CICD_PROJECT}
