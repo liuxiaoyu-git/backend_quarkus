f3a24edac39e254ba3d2f8ddb4dc0fd5a770b553
voravitl <voravitl@gmail.com>
2020-02-27 14:37:38 +0700

    update setup nexus script

--- bin/setup_ci_cd_tools.sh
+++ bin/setup_ci_cd_tools.sh
@@ -23,7 +23,7 @@ function check_pod(){
         fi
     done
 }
-CICD_PROJECT=ci-cd
+export CICD_PROJECT=ci-cd
 echo "########## Start build Jenkins ##########"
 oc new-project ${CICD_PROJECT} --display-name="CI/CD Tools"
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
@@ -32,28 +32,33 @@ oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=
 # check_pod "jenkins"
 echo "########## Start build Nexus   ##########"
 oc new-app sonatype/nexus3:latest --name=nexus -n ${CICD_PROJECT}
-oc expose svc nexus -n ${CICD_PROJECT}
+#oc expose svc nexus -n ${CICD_PROJECT}
+oc create route edge nexus --service=nexus --port=8081
 oc rollout pause dc nexus -n ${CICD_PROJECT}
 oc patch dc nexus --patch='{ "spec": { "strategy": { "type": "Recreate" }}}' -n ${CICD_PROJECT}
 oc set resources dc nexus --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m -n ${CICD_PROJECT}
-oc set volume dc/nexus --add --overwrite --name=nexus-pv-1 --mount-path=/nexus-data/ --type persistentVolumeClaim --claim-name=nexus-pvc --claim-size=4Gi -n ${CICD_PROJECT}
+oc set volume dc/nexus --add --overwrite --name=nexus-pv-1 --mount-path=/nexus-data/ --type persistentVolumeClaim --claim-name=nexus-pvc --claim-size=1Gi -n ${CICD_PROJECT}
 oc set probe dc/nexus --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok -n ${CICD_PROJECT}
 oc set probe dc/nexus --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/ -n ${CICD_PROJECT}
 oc rollout resume dc nexus -n ${CICD_PROJECT}
 check_pod "nexus"
-echo "##########   Setup Nexus   ############"
+echo "##########   Configure Nexus   ############"
+echo "Wait 10 sec..."
+sleep 10
 export NEXUS_POD=$(oc get pods | grep nexus | grep -v deploy | awk '{print $1}')
 export NEXUS_PASSWORD=$(oc rsh $NEXUS_POD cat /nexus-data/admin.password)
 # https://raw.githubusercontent.com/redhat-gpte-devopsautomation/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
 ./setup_nexus3.sh admin $NEXUS_PASSWORD http://$(oc get route nexus --template='{{ .spec.host }}')
+echo "expose port 5000 for container registry"
 oc expose dc nexus --port=5000 --name=nexus-registry
 oc create route edge nexus-registry --service=nexus-registry --port=5000
 echo "########   Setup SonarQube   ##########"
-oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db
+oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param VOLUME_CAPACITY=2Gi --labels=app=sonarqube_db
 check_pod "postgresql"
 oc new-app --docker-image=quay.io/gpte-devops-automation/sonarqube:7.9.1 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube
 oc rollout pause dc sonarqube
-oc expose service sonarqube
+#oc expose svc sonarqube
+oc create route edge sonarqube --service=sonarqube --port=9000
 oc set volume dc/sonarqube --add --overwrite --name=sonarqube-volume-1 --mount-path=/opt/sonarqube/data/ --type persistentVolumeClaim --claim-name=sonarqube-pvc --claim-size=1Gi
 oc set resources dc sonarqube --limits=memory=3Gi,cpu=2 --requests=memory=2Gi,cpu=1
 oc patch dc sonarqube --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'
@@ -77,5 +82,5 @@ echo "NEXUS Password = ${NEXUS_PASSWORD}"
 echo "Nexus password is stored at bin/nexus_password.txt"
 echo ${NEXUS_PASSWORD} > nexus_password.txt
 echo "Record this password and change it via web console"
-echo "Enable anonymous access"
+echo "Youn need to enable anonymous access"
 echo "SONARQUBE URL = $(oc get route sonarqube -n ${CICD_PROJECT} -o jsonpath='{.spec.host}') "
