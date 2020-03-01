aa8f158b8eb34db88f216179594fa428c9e3ac2e
voravitl <voravitl@gmail.com>
2020-03-01 22:18:24 +0700

    s2i - jvm

--- code/build_jvm_s2i.sh
+++ code/build_jvm_s2i.sh
@@ -1,22 +1,26 @@
 #!/bin/sh 
-set -x
 APP_NAME=backend
 BASE_IMAGE=registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift
 CONTEXT_DIR=code 
 APP_REPOSITORY=https://gitlab.com/ocp-demo/backend_quarkus.git
 
+echo "Check for .s2i/environment"
+if [ ! -f .s2i/environment ];
+then
+    echo ".s2i/environment is missing"
+    exit 1
+fi
 # To build the image on OpenShift
 oc new-app \
 ${BASE_IMAGE}~${APP_REPOSITORY} \
 --context-dir=${CONTEXT_DIR} \
 --name=${APP_NAME}
-
+echo "##### Wait for 3 sec #####"
+sleep 3
 # View build log
+echo "##### Build Log #####"
 oc logs -f bc/${APP_NAME}
-
 # To create the route
 oc expose svc/${APP_NAME}
-
 # Get the route URL
-# export URL="http://$(oc get route | grep ${APP_NAME} | awk '{print $2}')"
-# curl $URL
+echo "URL: http://$(oc get route | grep ${APP_NAME} | awk '{print $2}')"
