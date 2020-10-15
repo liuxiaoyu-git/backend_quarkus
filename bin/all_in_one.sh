bbced9ab3e9bdb61f4923bcf854d008ec61a1d92
voravitl <voravitl@gmail.com>
2020-10-15 11:30:54 +0700

    Use --as-deployment-config for OCP45

--- bin/all_in_one.sh
+++ bin/all_in_one.sh
@@ -11,6 +11,6 @@ banner "Create ci-cd,dev,stage,uat and prod project"
 banner "Build jenkins slave with maven36"
 ./setup_maven36_slave.sh
 banner "Start build CI/CD Tools"
-./setup_ci_cd_tools.sh
+./setup_ci_cd_tools_ocp45.sh
 banner "Create pipelines for backend app (Quarkus)"
 ./create_pipelines.sh
