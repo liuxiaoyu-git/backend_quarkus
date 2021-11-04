bf39b0f18e996fd9416b65328990ae401892cab4
voravitl <voravitl@gmail.com>
2021-11-04 09:44:09 +0700

    check for podman

--- code/build_jvm_container.sh
+++ code/build_jvm_container.sh
@@ -2,9 +2,15 @@
 CONTAINER_NAME=backend
 TAG=$1
 mvn clean package -DskipTests=true
+CONTAINER_RUNTIME=podman
+podman --version 1>/dev/null 2>&1
+if [ $? -ne 0 ];
+then
+   CONTAINER_RUNTIME=docker 
+fi
 # echo "*" > .dockerignore
 # echo "!target/*-runner" >>.dockerignore
 # echo "!target/*-runner.jar" >> .dockerignore
 # echo "!target/lib/*" >> .dockerignore
-docker build -f src/main/docker/Dockerfile.jvm \
+$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.jvm \
 -t ${CONTAINER_NAME}:${TAG} .
--- code/build_native_container.sh
+++ code/build_native_container.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 CONTAINER_NAME=backend-native
 TAG=$1
+CONTAINER_RUNTIME=podman
+podman --version 1>/dev/null 2>&1
+if [ $? -ne 0 ];
+then
+   CONTAINER_RUNTIME=docker
+fi
 # Use native container build
 START_BUILD_APP=$(date +%s)
 mvn clean package -Dquarkus.native.container-build=true \
 -DskipTests=true  -Pnative 
 END_BUILD_APP=$(date +%s)
 START_BUILD_CONTAINER=$(date +%s)
-docker build -f src/main/docker/Dockerfile.native \
+$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.native \
 -t ${CONTAINER_NAME}:${TAG} .
 END_BUILD_CONTAINER=$(date +%s)
 BUILD_APP=$(expr ${END_BUILD_APP} - ${START_BUILD_APP})
