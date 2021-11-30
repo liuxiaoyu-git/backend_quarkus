c2cd8457aab90c58908e5cf3ddcac4b41535ce65
voravitl <voravitl@gmail.com>
2021-11-30 13:21:06 +0700

    ubi-minimal latest

--- code/build_jvm_container.sh
+++ code/build_jvm_container.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 CONTAINER_NAME=backend
+if [ $# -lt 1 ];
+then
+   echo "Usage: build_jvm_container.sh <TAG>"
+   exit 1
+fi
 TAG=$1
+echo "Build with tag $TAG"
 mvn clean package -DskipTests=true
 CONTAINER_RUNTIME=podman
 podman --version 1>/dev/null 2>&1
@@ -8,9 +14,5 @@ if [ $? -ne 0 ];
 then
    CONTAINER_RUNTIME=docker 
 fi
-# echo "*" > .dockerignore
-# echo "!target/*-runner" >>.dockerignore
-# echo "!target/*-runner.jar" >> .dockerignore
-# echo "!target/lib/*" >> .dockerignore
 $CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.jvm \
 -t ${CONTAINER_NAME}:${TAG} .
