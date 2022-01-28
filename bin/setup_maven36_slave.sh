877d7b268bdf6c8a54ce9dd4e7612f7629660273
voravitl <voravitl@gmail.com>
2022-01-28 09:57:04 +0700

    CVE and remove microdnf

--- bin/setup_maven36_slave.sh
+++ bin/setup_maven36_slave.sh
@@ -4,7 +4,7 @@ JENKINS_SLAVE=maven36-with-tools
 MAVEN_VERSION=3.6.3
 JMETER_VERSION=5.4.1
 echo "################  ${JENKINS_SLAVE} ##################"
-oc new-build --strategy=docker -D $'FROM quay.io/openshift/origin-jenkins-agent-maven:4.1\n
+oc new-build --strategy=docker -D $'FROM quay.io/openshift/origin-jenkins-agent-maven:4.10.0 \n
    USER root\n
    RUN curl https://copr.fedorainfracloud.org/coprs/alsadi/dumb-init/repo/epel-7/alsadi-dumb-init-epel-7.repo -o /etc/yum.repos.d/alsadi-dumb-init-epel-7.repo && \ \n
    curl https://raw.githubusercontent.com/cloudrouter/centos-repo/master/CentOS-Base.repo -o /etc/yum.repos.d/CentOS-Base.repo && \ \n
@@ -15,6 +15,10 @@ oc new-build --strategy=docker -D $'FROM quay.io/openshift/origin-jenkins-agent-
    rm -f /tmp/apache-maven-3.6.3-bin.tar && \ \n
    chmod -R 755 /opt/apache-maven-3.6.3 && \ \n
    chown -R 1001:0 /opt/apache-maven-3.6.3 && \ \n
+   mkdir -p /opt/rox && \ \n
+   curl -L -o /opt/rox/roxctl https://mirror.openshift.com/pub/rhacs/assets/latest/bin/Linux/roxctl && \ \n
+   chmod -R 755 /opt/rox/roxctl && \ \n
+   chown -R 1001:0 /opt/rox/roxctl && \ \n
    curl -L -o /tmp/nexus-cli https://s3.eu-west-2.amazonaws.com/nexus-cli/1.0.0-beta/linux/nexus-cli  && \ \n
    mkdir -p /opt/nexus-cli && \ \n
    mv /tmp/nexus-cli /opt/nexus-cli && \ \n
@@ -26,8 +30,8 @@ oc new-build --strategy=docker -D $'FROM quay.io/openshift/origin-jenkins-agent-
    chmod -R 755 /opt/apache-jmeter-5.4.1 && \ \n
    chown -R 1001:0 /opt/apache-jmeter-5.4.1 && \ \n
    DISABLES="--disablerepo=rhel-server-extras --disablerepo=rhel-server --disablerepo=rhel-fast-datapath --disablerepo=rhel-server-optional --disablerepo=rhel-server-ose --disablerepo=rhel-server-rhscl" && \ \n
-   yum $DISABLES -y --setopt=tsflags=nodocs install skopeo && yum clean all   \n
-   ENV PATH=/opt/apache-maven-3.6.3/bin:/opt/nexus-cli:/opt/apache-jmeter-5.4.1/bin:$PATH \n
+   yum $DISABLES -y --setopt=tsflags=nodocs install skopeo jq && yum clean all   \n
+   ENV PATH=/opt/apache-maven-3.6.3/bin:/opt/nexus-cli:/opt/apache-jmeter-5.4.1/bin:/opt/rox:$PATH \n
    USER 1001' --name=${JENKINS_SLAVE} -n ${PROJECT}
 # uid=1000680000(default) gid=0(root) groups=0(root),1000680000
 # 
