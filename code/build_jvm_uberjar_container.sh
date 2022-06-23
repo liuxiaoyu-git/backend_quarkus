55a797440e88da35bd9b4d4eca97f483dafc1f24
Voravit <voravitl@gmail.com>
2022-06-23 09:10:52 +0700

    uber-jar

--- code/build_jvm_uberjar_container.sh
+++ code/build_jvm_uberjar_container.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-CONTAINER_NAME=backend-uber
-TAG=$1
+CONTAINER_NAME=backend
+TAG=uber-jar
+CONTAINER_RUNTIME=podman
+podman --version 1>/dev/null 2>&1
+if [ $? -ne 0 ];
+then
+   CONTAINER_RUNTIME=docker 
+fi
 mvn clean package -DskipTests=true -Dquarkus.package.type=uber-jar
-echo "*" > .dockerignore
-echo "!target/*-runner" >>.dockerignore
-echo "!target/*-runner.jar" >> .dockerignore
-echo "!target/lib/*" >> .dockerignore
-docker build -f src/main/docker/Dockerfile.jvm_uberjar \
+$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.jvm_uberjar \
 -t ${CONTAINER_NAME}:${TAG} .
-rm -f .dockerignore
