d0afaa32d2eab88756b0a22f63d77e5959f16efe
Voravit <voravitl@gmail.com>
2022-07-08 08:33:44 +0700

    build with 3 base image

--- code/build_native_container.sh
+++ code/build_native_container.sh
@@ -16,11 +16,15 @@ fi
 START_BUILD_APP=$(date +%s)
 mvn $MAVEN_CLI
 END_BUILD_APP=$(date +%s)
-START_BUILD_CONTAINER=$(date +%s)
+#START_BUILD_CONTAINER=$(date +%s)
 $CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.native \
 -t ${CONTAINER_NAME}:${TAG} .
-END_BUILD_CONTAINER=$(date +%s)
+$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.native-distroless \
+-t ${CONTAINER_NAME}:${TAG}-distroless .
+$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.native-micro \
+-t ${CONTAINER_NAME}:${TAG}-micro .
+#END_BUILD_CONTAINER=$(date +%s)
 BUILD_APP=$(expr ${END_BUILD_APP} - ${START_BUILD_APP})
-BUILD_CONTAINER=$(expr ${END_BUILD_CONTAINER} - ${START_BUILD_CONTAINER})
+#BUILD_CONTAINER=$(expr ${END_BUILD_CONTAINER} - ${START_BUILD_CONTAINER})
 echo "Elasped time to build app:${BUILD_APP} sec"
 echo "Elasped time to build container:${BUILD_CONTAINER} sec"
