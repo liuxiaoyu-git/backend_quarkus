43f2d13d7c3f218ffbbcdae622741c680ffbbf3e
Voravit <voravitl@gmail.com>
2022-07-24 20:28:35 +0700

    5gb memory

--- code/build_native_container.sh
+++ code/build_native_container.sh
@@ -9,20 +9,20 @@ then
   CONTAINER_RUNTIME=docker
   MAVEN_CLI="clean package -Dquarkus.native.container-build=true -DskipTests=true  -Pnative"
 else
-  #MAVEN_CLI="clean package -Pnative -Dquarkus.native.container-build=true -Dquarkus.native.remote-container-build=true -Dquarkus.native.container-runtime=podman -Dquarkus.native.native-image-xmx=5g "
-  MAVEN_CLI="clean package -Pnative -Dquarkus.native.container-build=true -Dquarkus.native.remote-container-build=true -Dquarkus.native.container-runtime=podman"
-  #MAVEN_CLI="clean package -Dnative -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=podman -DskipTests=true"
+  MAVEN_CLI="clean package -Pnative -Dquarkus.native.container-build=true -Dquarkus.native.remote-container-build=true -Dquarkus.native.container-runtime=podman -Dquarkus.native.native-image-xmx=5g "
+  #MAVEN_CLI="clean package -Pnative -Dquarkus.native.container-build=true -Dquarkus.native.remote-container-build=true -Dquarkus.native.container-runtime=podman"
+  ##MAVEN_CLI="clean package -Dnative -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=podman -DskipTests=true"
 fi
 START_BUILD_APP=$(date +%s)
 mvn $MAVEN_CLI
 END_BUILD_APP=$(date +%s)
 #START_BUILD_CONTAINER=$(date +%s)
-$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.native \
--t ${CONTAINER_NAME}:${TAG} .
+#$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.native \
+#-t ${CONTAINER_NAME}:${TAG} .
 $CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.native-distroless \
 -t ${CONTAINER_NAME}:${TAG}-distroless .
-$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.native-micro \
--t ${CONTAINER_NAME}:${TAG}-micro .
+#$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.native-micro \
+#-t ${CONTAINER_NAME}:${TAG}-micro .
 #END_BUILD_CONTAINER=$(date +%s)
 BUILD_APP=$(expr ${END_BUILD_APP} - ${START_BUILD_APP})
 #BUILD_CONTAINER=$(expr ${END_BUILD_CONTAINER} - ${START_BUILD_CONTAINER})
