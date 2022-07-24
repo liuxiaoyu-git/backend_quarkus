11603895ed1c9e59d9b889a51b7cc2cd30dfa524
Voravit <voravitl@gmail.com>
2022-07-24 16:29:18 +0700

    buildah

--- code/build_jvm_container_micro.sh
+++ code/build_jvm_container_micro.sh
@@ -1,42 +1,39 @@
 #!/bin/sh
-echo "Build Base Image"
-JAVA_PACKAGE=java-11-openjdk-headless
-RUN_JAVA_VERSION=1.3.8
-LANG='en_US.UTF-8'
-LANGUAGE='en_US:en'
-microcontainer=$(buildah from registry.access.redhat.com/ubi8/ubi-micro)
-micromount=$(buildah mount $microcontainer)
-microjavajdk=ubi-micro-openjdk11:latest
-yum install --installroot $micromount --nodocs -y \
-    ca-certificates ${JAVA_PACKAGE} curl
-yum clean all --installroot $micromount
+๒
+yum clean all --installroot $microcontainer 
 buildah config --env JAVA_PACKAGE="java-11-openjdk-headless" --env LANG="en_US.UTF-8" \
-        --env RUN_JAVA_VERSION="1.3.8" --env LANGUAGE="en_US:en" \--env JAVA_OPTIONS="-Dquarkus.http.host=0.0.0.0 -Djava.util.logging.manager=org.jboss.logmanager.LogManager" \
+        --env RUN_JAVA_VERSION="1.3.8" --env LANGUAGE="en_US:en" \
+        --env JAVA_OPTIONS="-Dquarkus.http.host=0.0.0.0 -Djava.util.logging.manager=org.jboss.logmanager.LogManager" \
         $microcontainer
 buildah run $microcontainer mkdir /deployments
-buildah run $microcontainer curl https://repo1.maven.org/maven2/io/fabric8/run-java-sh/${RUN_JAVA_VERSION}/run-java-sh-${RUN_JAVA_VERSION}-sh.sh -o /deployments/run-java.sh
+buildah copy $microcontainer target/quarkus-app/lib/ /deployments
+buildah copy $microcontainer target/quarkus-app/*.jar /deployments
+buildah copy $microcontainer target/quarkus-app/app/ /deployments
+buildah copy $microcontainer target/quarkus-app/quarkus/ /deployments
+curl https://repo1.maven.org/maven2/io/fabric8/run-java-sh/${RUN_JAVA_VERSION}/run-java-sh-${RUN_JAVA_VERSION}-sh.sh -o ./run-java.sh
+buildah copy $microcontainer ./run-java.sh /deployments
 buildah run $microcontainer echo "securerandom.source=file:/dev/urandom" >> /etc/alternatives/jre/conf/security/java.security
-buildah run $microcontainer chown 1001 /deployments/run-java.sh
+buildah run $microcontainer chown -R 1001 /deployments
+#buildah run $microcontainer chown 1001 /deployments/run-java.sh
 buildah run $microcontainer chmod 540 /deployments/run-java.sh
-yum remove --installroot $micromount -y curl
+buildah config --port 8080 $microcontainer
+buildah config --entrypoint "/deployments/run-java.sh" $microcontainer
+buildah config --user 1001 $microcontainer
 buildah umount $microcontainer
-buildah commit $microcontainer $microjavajdk
-
-CONTAINER_NAME=backend
-TAG=micro
+buildah commit $microcontainer $backendimage
 # if [ $# -lt 1 ];
 # then
 #    echo "Usage: build_jvm_container.sh <TAG>"
 #    exit 1
 # fi
 # TAG=$1
-echo "Build with tag $TAG"
-mvn clean package -DskipTests=true
-CONTAINER_RUNTIME=podman
-podman --version 1>/dev/null 2>&1
-if [ $? -ne 0 ];
-then
-   CONTAINER_RUNTIME=docker
-fi
-$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.jvm.micro \
--t ${CONTAINER_NAME}:${TAG} .
+# echo "Build with tag $TAG"
+# mvn clean package -DskipTests=true
+# CONTAINER_RUNTIME=podman
+# podman --version 1>/dev/null 2>&1
+# if [ $? -ne 0 ];
+# then
+#    CONTAINER_RUNTIME=docker
+# fi
+# $CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.jvm.micro \
+# -t ${CONTAINER_NAME}:${TAG} .
