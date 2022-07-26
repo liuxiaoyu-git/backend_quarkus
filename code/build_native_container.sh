1a5bbe96b43753303d468d98a547466f91868aff
Voravit <voravitl@gmail.com>
2022-07-26 11:08:06 +0700

    Use UBI8

--- code/build_native_container.sh
+++ code/build_native_container.sh
@@ -14,17 +14,17 @@ else
   ##MAVEN_CLI="clean package -Dnative -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=podman -DskipTests=true"
 fi
 START_BUILD_APP=$(date +%s)
-mvn $MAVEN_CLI
+#mvn $MAVEN_CLI
 END_BUILD_APP=$(date +%s)
-#START_BUILD_CONTAINER=$(date +%s)
-#$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.native \
-#-t ${CONTAINER_NAME}:${TAG} .
+START_BUILD_CONTAINER=$(date +%s)
+$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.native \
+-t ${CONTAINER_NAME}:${TAG} .
 $CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.native-distroless \
 -t ${CONTAINER_NAME}:${TAG}-distroless .
-#$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.native-micro \
-#-t ${CONTAINER_NAME}:${TAG}-micro .
-#END_BUILD_CONTAINER=$(date +%s)
+$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.native-micro \
+-t ${CONTAINER_NAME}:${TAG}-micro .
+END_BUILD_CONTAINER=$(date +%s)
 BUILD_APP=$(expr ${END_BUILD_APP} - ${START_BUILD_APP})
-#BUILD_CONTAINER=$(expr ${END_BUILD_CONTAINER} - ${START_BUILD_CONTAINER})
+BUILD_CONTAINER=$(expr ${END_BUILD_CONTAINER} - ${START_BUILD_CONTAINER})
 echo "Elasped time to build app:${BUILD_APP} sec"
 echo "Elasped time to build container:${BUILD_CONTAINER} sec"
